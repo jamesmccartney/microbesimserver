7ee98099ead86af808e952c5e9c47ef821117e31
jmccartney <jmccartney@localhost>
2014-03-18 16:28:22 -0400

    Added to test scripting.

---
+++ src/microbesimserver/script_entity.js
@@ -0,0 +1,54 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * Entity Base
+ */
+
+//Basic GOComponents that make up this type
+//var entityComponents = 
+
+
+/*
+ * Decoration Base
+ */
+
+//Basic GOComponents that make up this type
+var decorationComponents = ("physics");
+
+
+/*
+ * Prop Base
+ */
+
+//Basic GOComponents that make up this type
+var propComponents = ("physics");
+
+
+/*
+ * Zone Base
+ */
+
+//Basic GOComponents that make up this type
+var zoneComponents = ("physics");
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getComponents(goType) {
+
+}
+
+
